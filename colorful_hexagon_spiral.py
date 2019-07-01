408bf18643b7874a5eedec0b3af3229a724ad320
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-07-02 00:18:56 +0530

    Update colorful_hexagon_spiral.py

--- colorful_hexagon_spiral.py
+++ colorful_hexagon_spiral.py
@@ -1,7 +1,8 @@
-from turtle import *
+import turtle
+shape = turtle.Turtle()
 colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
 for x in range(360):
-    pencolor(colors[x % 6])
-    width(x / 100 + 1)
-    forward(x)
-    left(59)
+    shape.pencolor(colors[x % 6])
+    shape.width(x / 100 + 1)
+    shape.forward(x)
+    shape.left(59)
